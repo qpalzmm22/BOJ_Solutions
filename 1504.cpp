51cbd75b315c7ce224812fcb6151f970d4de8aab
qpalzmm22 <qpalzmm22@gmail.com>
2021-12-28 15:21:06 +0900

    solving 1504

---
+++ 1504.cpp
@@ -0,0 +1,17 @@
+// 1504
+// Possibly find Shortest Path to 2 first then 3, then from 3 to 4. DP
+
+#include <iostream>
+
+using namespace std;
+
+vector<pair<int,int>> adj[800]; // adj list
+
+int main()
+{
+    int N, E;
+
+    cin >> N;
+    
+    return 0;
+}
